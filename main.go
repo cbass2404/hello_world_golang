8a46702a4ede82f4f4900a6258d2ba77ef23f505
Cory Bass <cory.bass.07@gmail.com>
2023-09-09 12:25:29 -0500

    first handler setup

--- main.go
+++ main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// fmt.Println("Hello, world")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello, world!")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes wrttien: %d", n))
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
+}
